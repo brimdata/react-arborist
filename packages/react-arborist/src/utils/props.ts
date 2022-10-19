015cb621a5d5968da8bccfefa07d5fc36816c3ce
James Kerr <kerr@hey.com>
2022-10-19 16:51:06 -0700

    Delete old function

--- packages/react-arborist/src/utils/props.ts
+++
@@ -1,8 +0,0 @@
-import { TreeProps } from "../types/tree-props";
-import { IdObj } from "../types/utils";
-
-export function validateProps<T extends IdObj>(
-  props: TreeProps<T>
-): TreeProps<T> {
-  return props;
-}
